b670cf9a81177420abdf95e0df026d5be25a030b
Omer Kaan Ayvaz <kaanayvaz00@gmail.com>
2023-05-24 16:52:04 +0300

    trying to learn sklearn

---
+++ DAY_2/predicting_stock_price.py
@@ -0,0 +1,46 @@
+import csv
+import numpy as np
+from sklearn.svm import SVR
+import matplotlib.pyplot as plt
+
+dates = []
+prices = []
+
+def get_data(filename):
+    with open(filename, "r") as csvfile:
+        csvFileReader = csv.reader(csvfile)
+        next(csvFileReader)
+        for row in csvFileReader:
+            dates.append(int(row[0].split('-')[0]))
+            prices.append(float(row[1]))
+    return
+
+def predict_prices(dates,prices):
+    dates = np.reshape(dates, (len(dates), 1))
+    svr_len = SVR(kernel= "linear", C=1e3)
+    #svr_poly = SVR(kernel= "poly", C=1e3, degree=2)
+    svr_rbf = SVR(kernel= "rbf", C=1e3, gamma=0.1)
+    svr_len.fit(dates, prices)
+    #svr_poly.fit(dates, prices) #it did not work I did not understand why
+    svr_rbf.fit(dates, prices)
+
+    plt.scatter(dates, prices, color = "black", label = "Data")
+    plt.plot(dates, svr_rbf.predict(dates), color = "red", label = "RBF model")
+    plt.plot(dates, svr_len.predict(dates), color = "green", label = "Linear model")
+
+    plt.xlabel("Date")
+    plt.ylabel("Price")
+    plt.title("Support Vector Regression")
+
+    plt.legend()
+    plt.show()
+
+get_data("/Users/omerkaanayvaz/Desktop/python_bu_sefer_bitti/1000daymachinelearning/DAY_2/AAPL (1).csv")
+
+predict_prices(dates,prices)
+
+
+
+
+
+
